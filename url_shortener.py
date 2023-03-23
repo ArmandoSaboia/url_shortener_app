471680123d01efbad1d4153acc2a57b386c92512
Armando Saboia <4rm4nd1nh0_br@protonmail.com>
2023-03-23 13:08:02 -0300

    Update url_shortener.py

--- url_shortener.py
+++ url_shortener.py
@@ -18,3 +18,34 @@ def main():
 		    
 if __name__ == '__main__':
     main()
+
+import dash
+
+app = dash.Dash(__name__)
+
+app.index_string = """<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-131327483-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-131327483-1');
+        </script>
+        {%metas%}
+        <title>{%title%}</title>
+        {%favicon%}
+        {%css%}
+    </head>
+    <body>
+        {%app_entry%}
+        <footer>
+            {%config%}
+            {%scripts%}
+            {%renderer%}
+        </footer>
+    </body>
+</html>"""
